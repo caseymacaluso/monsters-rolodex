e2a7137e74deee021648a092a75c4f1c2f0f2f23
Casey Macaluso <macaluso.casey@gmail.com>
2022-09-06 17:20:25 -0500

    add comments, performance improvements

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import { Component } from "react";
 import "./App.css";
 
 class App extends Component {
+  // runs when component is initialized (first in the cycle)
   constructor() {
-    super();
+    super(); // calls constructor() method of any other classes the component is extending from (in this case, Component)
+    // initializes state
     this.state = {
       monsters: [],
       searchField: "",
@@ -13,8 +15,12 @@ class App extends Component {
     console.log("constructor");
   }
 
+  // Lifecycle method
+  // Fetches data the moment the App component is 'mounted' on the DOM, i.e. rendered
+  // Runs after the render() method
   componentDidMount() {
     console.log("componentDidMount");
+    // API call, parses to JSON, then sets the value of monsters to the parsed information
     fetch("https://jsonplaceholder.typicode.com/users")
       .then(response => response.json())
       .then(users =>
@@ -24,13 +30,25 @@ class App extends Component {
       );
   }
 
+  // Handler to handle changes in the search field
+  onSearchChange = evt => {
+    const searchField = evt.target.value.toLowerCase();
+    this.setState(() => {
+      return { searchField };
+    });
+  };
+
   render() {
+    // render runs after the constructor
     console.log("render");
 
-    const filteredMonsters = this.state.monsters.filter(monster => {
-      return monster.name
-        .toLowerCase()
-        .includes(this.state.searchField.toLowerCase());
+    // Pulling state variables & functions from 'this';
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+
+    // Filters the monsters array to match the string input in the search input field
+    const filteredMonsters = monsters.filter(monster => {
+      return monster.name.toLowerCase().includes(searchField.toLowerCase());
     });
 
     return (
@@ -39,12 +57,7 @@ class App extends Component {
           className="search"
           type="search"
           placeholder="search monsters"
-          onChange={evt => {
-            const searchField = evt.target.value.toLowerCase();
-            this.setState(() => {
-              return { searchField };
-            });
-          }}
+          onChange={onSearchChange}
         />
         {filteredMonsters.map(monster => {
           return (
