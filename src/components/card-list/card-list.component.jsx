6ebd4fdb8246997ed4b2e6831c246635f2a4c8eb
Casey Macaluso <macaluso.casey@gmail.com>
2022-09-07 13:38:38 -0500

    convert Card, CardList to functional components

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,19 +1,31 @@
-import { Component } from "react";
+// import { Component } from "react";
 import Card from "../card/card.component";
-
 import "./card-list.styles.css";
 
-class CardList extends Component {
-  render() {
-    const { monsters } = this.props;
-    return (
-      <div className="card-list">
-        {monsters.map(monster => {
-          return <Card monster={monster} key={monster.id} />;
-        })}
-      </div>
-    );
-  }
-}
+// FUNCTION-BASED APPROACH
+const CardList = props => {
+  const { monsters } = props;
+  return (
+    <div className="card-list">
+      {monsters.map(monster => {
+        return <Card monster={monster} key={monster.id} />;
+      })}
+    </div>
+  );
+};
+
+// CLASS-BASED APPROACH
+// class CardList extends Component {
+//   render() {
+//     const { monsters } = this.props;
+//     return (
+//       <div className="card-list">
+//         {monsters.map(monster => {
+//           return <Card monster={monster} key={monster.id} />;
+//         })}
+//       </div>
+//     );
+//   }
+// }
 
 export default CardList;
--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,21 +1,36 @@
-import { Component } from "react";
-
+// import { Component } from "react";
 import "./card.styles.css";
 
-class Card extends Component {
-  render() {
-    const { name, email, id } = this.props.monster;
-    return (
-      <div className="card-container">
-        <img
-          alt={`monster ${name}`}
-          src={`https://robohash.org/${id}?set=set2&size=180x180`}
-        />
-        <h2>{name}</h2>
-        <p>{email}</p>
-      </div>
-    );
-  }
-}
+//FUNCTION-BASED APPROACH
+const Card = props => {
+  const { name, email, id } = props.monster;
+  return (
+    <div className="card-container">
+      <img
+        alt={`monster ${name}`}
+        src={`https://robohash.org/${id}?set=set2&size=180x180`}
+      />
+      <h2>{name}</h2>
+      <p>{email}</p>
+    </div>
+  );
+};
+
+// CLASS-BASED APPROACH
+// class Card extends Component {
+//   render() {
+//     const { name, email, id } = this.props.monster;
+//     return (
+//       <div className="card-container">
+//         <img
+//           alt={`monster ${name}`}
+//           src={`https://robohash.org/${id}?set=set2&size=180x180`}
+//         />
+//         <h2>{name}</h2>
+//         <p>{email}</p>
+//       </div>
+//     );
+//   }
+// }
 
 export default Card;
